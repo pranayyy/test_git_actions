4dbd3a1840f27dbfd2da30127a87188a414d3581
pranayyy <pranaireddy007@gmail.com>
2024-09-27 10:32:58 +0530

    first commit from vs

---
+++ lambda_function.py
@@ -0,0 +1,24 @@
+import unittest
+
+# lambda_function.py
+
+# Define a basic lambda function
+add = lambda x, y: x + y
+
+# Unit test cases for the lambda function
+
+class TestLambdaFunction(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(add(1, 2), 3)
+    
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-1, -2), -3)
+    
+    def test_add_mixed_numbers(self):
+        self.assertEqual(add(-1, 2), 1)
+    
+    def test_add_zeros(self):
+        self.assertEqual(add(0, 0), 0)
+
+if __name__ == '__main__':
+    unittest.main()
